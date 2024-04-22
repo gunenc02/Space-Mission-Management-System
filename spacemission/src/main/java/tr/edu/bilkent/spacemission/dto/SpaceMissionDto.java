9185816c5991179a7ef44bbe038f0327c5a95b95
Musa Yiğit Yayla <yigit.yayla@ug.bilkent.edu.tr>
2024-04-22 23:53:12 +0300

    Implementation of mark mission performed no test
    Since I am not familiar with Spring Boot query execution syntax I just wrote the queries now. Also these queries are not present in project report so we should test them. Project runs in my local machine without a problem. I made no testing however.

--- spacemission/src/main/java/tr/edu/bilkent/spacemission/dto/SpaceMissionDto.java
+++ spacemission/src/main/java/tr/edu/bilkent/spacemission/dto/SpaceMissionDto.java
@@ -1,4 +1,16 @@
 package tr.edu.bilkent.spacemission.dto;
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class SpaceMissionDto {
+    long missionId;
+    String missionName;
+    String missionObjective;
+    String createDate;
+    double budget;
+    long creatorId;
 }
--- spacemission/src/main/java/tr/edu/bilkent/spacemission/repository/CompanyRepository.java
+++ spacemission/src/main/java/tr/edu/bilkent/spacemission/repository/CompanyRepository.java
@@ -1,5 +1,6 @@
 package tr.edu.bilkent.spacemission.repository;
 
+import java.util.List;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import tr.edu.bilkent.spacemission.dto.CompanyDto;
@@ -48,5 +49,22 @@ public class CompanyRepository {
     }
 
     public void offerJob(long astronautId) {
+    }
+
+    /*
+    *@param missionId id of the space mission to be performed
+    * returns whether the mission has been marked as performed in the database
+     */
+    public boolean markSpaceMissionAsPerformed(long missionId){
+        //execute such a query that if a space mission with given attributes exists try and mark it as performed
+        //first check if there exists an entry in space mission performings with specified mission id and not yet performed
+        final String notPerformedStatus = "???Determine this";
+        final String performedStatus = "???AlsoDetermine";
+        String query = "SELECT * FROM space_mission_performings " +
+                        "WHERE space_mission_id = :missionId AND perform_status = :notPerformedStatus;";
+        //execute the first query and if retrieval yields something then update that entry
+        query = "UPDATE space_mission_performings SET perform_status = :performedStatus " +
+                "WHERE space_mission_id = :missionId;";
+        return false;
     }
 }
