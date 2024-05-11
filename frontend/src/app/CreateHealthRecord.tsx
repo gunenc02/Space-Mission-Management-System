360cac910315c2053e0588f5648306db15eef13a
Musa Yiğit Yayla <yigit.yayla@ug.bilkent.edu.tr>
2024-05-11 22:39:36 +0300

    Further implementation on CHR.tsx

--- frontend/src/app/CreateHealthRecord.tsx
+++ frontend/src/app/CreateHealthRecord.tsx
@@ -4,20 +4,11 @@ import React from 'react';
 const CHR_WIDTH = 400;
 const CHR_HEIGHT = 600;
 
+
 function cancelHandler(){
 
 }
-function registerHandler(){
-    const formData = {
-        height: document.getElementById('tfHeight')?.nodeValue || '', //?.nodeValue syntax allows us to avoid generating errors if the input is null
-        weight: document.getElementById('tfWeight')?.nodeValue || '',
-        heartRate: document.getElementById('tfHeartRate')?.nodeValue || '',
-        bloodPressure: document.getElementById('tfBloodPressure')?.nodeValue || '',
-        vaccinations: (document.getElementById('tfVaccinations') as HTMLTextAreaElement)?.value || '',
-        notes: (document.getElementById('tfNotes') as HTMLTextAreaElement)?.value || '',
-        eligible: (document.querySelector('input[name="eligible"]:checked') as HTMLInputElement)?.value || ''
-    };
-}
+
 function modalClose(){
 
 }
@@ -26,11 +17,32 @@ function fetchSuccess(){
 }
 
 export default function CreateHealthRecord(astronautId){
+    const [errVisible, setErrVisible] = useState<boolean>(false);
     
+    const registerHandler = function (){
+        console.log("CHR debug: registerHandler invoked");
+        const formData = {
+            height: document.getElementById('tfHeight')?.nodeValue || '', //?.nodeValue syntax allows us to avoid generating errors if the input is null
+            weight: document.getElementById('tfWeight')?.nodeValue || '',
+            heartRate: document.getElementById('tfHeartRate')?.nodeValue || '',
+            bloodPressure: document.getElementById('tfBloodPressure')?.nodeValue || '',
+            vaccinations: (document.getElementById('tfVaccinations') as HTMLTextAreaElement)?.value || '',
+            notes: (document.getElementById('tfNotes') as HTMLTextAreaElement)?.value || '',
+            eligible: (document.querySelector('input[name="eligible"]:checked') as HTMLInputElement)?.value || ''
+        };
+        //evaluate formData to ensure that retrieved input is okay
+        if(formData.height !== '' && formData.weight != '' && formData.heartRate != '' && formData.bloodPressure != '' && formData.vaccinations != '' && formData.eligible != ''){
+            setErrVisible(false);
+            
+        }
+        else{
+            setErrVisible(true);
+        }
+    }
     return(
         <div style={{width: CHR_WIDTH, height: CHR_HEIGHT}}>
             <label htmlFor="tfHeight">Height:</label> <br></br>
-            <input type="text" id="tfHeight" onChange={updateState}></input> <br></br>
+            <input type="text" id="tfHeight"></input> <br></br>
             <label htmlFor="tfWeight">Weight:</label> <br></br>
             <input type="text" id="tfWeight"></input> <br></br>
 
@@ -39,7 +51,7 @@ export default function CreateHealthRecord(astronautId){
             <label htmlFor="tfBloodPressure">Blood Pressure:</label> <br></br>
             <input type="text" id="tfBloodPressure"></input> <br></br>
 
-            <label htmlFor="tfVaccinations">Vaccinations</label> <br></br>
+            <label htmlFor="tfVaccinations">Vaccinations (, separated list)</label> <br></br>
             <textarea id="tfVaccinations"></textarea> <br></br>
 
             <label htmlFor="tfNotes">Notes</label> <br></br>
@@ -50,9 +62,10 @@ export default function CreateHealthRecord(astronautId){
             <input type="radio" id="radiono" name="eligible"></input> <br></br>
             <label htmlFor="radioYes">Yes</label>
             <label htmlFor="radioNo">No</label> <br></br>
+            <h5 style={{ color: 'red', visibility: errVisible ? 'visible' : 'hidden' }}>Fields (except notes) cannot be empty</h5>
 
-            <button onInput={cancelHandler} style={{backgroundColor: 'red', color: 'white'}}>Cancel</button>
-            <button onInput={registerHandler} style={{backgroundColor: 'green', color: 'white'}}>Register</button>
+            <button onClick={cancelHandler} style={{backgroundColor: 'red', color: 'white'}}>Cancel</button>
+            <button onClick={registerHandler} style={{backgroundColor: 'green', color: 'white'}}>Register</button>
         </div>
     );
 }
