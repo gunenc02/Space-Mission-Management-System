b97ea75dcea1d5f3afa5d3cef4bee896b3542f2b
emirtuglu <tugluemir@gmail.com>
2024-05-11 18:12:00 +0300

    Add menu to header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,9 +1,42 @@
+import { Link } from "react-router-dom";
 import "../styles/Header.css";
+import { useState } from "react";
 
 export default function Header() {
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
+
   return (
     <div className="header-outer">
-      <h1>Space Mission System</h1>
+      <div
+        className="header-profile-outer"
+        onClick={() => setDropdownOpen(!isDropdownOpen)}
+      >
+        <div className="header-profile">
+          <div className="header-profile-text">
+            <span>Menu</span>
+          </div>
+
+          {isDropdownOpen && (
+            <div className="header-profile-dropdown">
+              <Link to="/myprofile" className="header-profile-dropdown-link">
+                View Profile
+              </Link>
+              <Link to="/bids" className="header-profile-dropdown-link">
+                Bids
+              </Link>
+              <Link to="/settings" className="header-profile-dropdown-link">
+                Settings
+              </Link>
+              <Link to="/login" className="header-profile-dropdown-link">
+                Logout
+              </Link>
+            </div>
+          )}
+        </div>
+      </div>
+      <div>
+        <h1 className="header-header">Space Mission System</h1>
+      </div>
     </div>
   );
 }
