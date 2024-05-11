6301f50d030fbae8dddb3a60437aa366283c3099
emirtuglu <tugluemir@gmail.com>
2024-05-11 15:36:54 +0300

    Complete astronauts list page

---
+++ frontend/src/data-types/props.tsx
@@ -0,0 +1,48 @@
+export interface Astronaut {
+  id: number;
+  name: string;
+  image: string;
+  dateOfBirth: Date;
+  onDuty: boolean;
+  country: string;
+  salary: number;
+}
+
+export interface Agency {
+  id: number;
+  name: string;
+  mail: string;
+  logo: string;
+  status: boolean;
+}
+
+export interface Company {
+  id: number;
+  name: string;
+  mail: string;
+  country: string;
+  logo: string;
+  budget: number;
+}
+
+export interface SpaceMission {
+  id: number;
+  missionName: string;
+  image: string;
+  objective: string;
+  budget: number;
+  createDate: Date;
+  performDate: Date;
+  platformId: number;
+  creatorId: number;
+  performerId: number;
+  performStatus: string;
+}
+
+export interface Platform {
+  id: number;
+  platformName: string;
+  productionYear: number;
+  image: string;
+  costPerLaunch: number;
+}
