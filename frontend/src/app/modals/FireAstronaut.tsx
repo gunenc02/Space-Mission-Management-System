4a33b757de014f7e3cc538e2d9b3119166c5f722
Musa Yiğit Yayla <yigit.yayla@ug.bilkent.edu.tr>
2024-05-14 21:21:59 +0300

    FireAstronaut fetch frontend implemented

--- frontend/src/app/modals/FireAstronaut.tsx
+++ frontend/src/app/modals/FireAstronaut.tsx
@@ -4,7 +4,34 @@ import "../../styles/Modal.css";
 
 export default function FireAstronaut(props: FireAstronautProps) {
   const handleFire = function(){
-    const deleteUrl = "http://localhost:8080/";
+    const deleteUrl = "http://localhost:8080/fireAstronaut";
+    const performerId = "??????????companyId" // company id we must retrieve via login
+    const requestBody = {
+      id: performerId,
+      astronautId: props.astronautId,
+    }
+
+    fetch(deleteUrl, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(requestBody),
+    }).then((response) => {
+      if(!response.ok){
+        throw new Error("Network respone was not ok");
+      }
+      else{
+        //delete successful close the modal
+        props.onClose();
+      }
+    }).catch((error) => {
+      // Handle errors here
+      console.error(
+        "There was a problem with the fetch (delete (fireAstronaut)) operation:",
+        error
+      );
+    });
   }
 
   //FIX BACKEND FIRST TO IMPLEMENT FETCH OF THIS
@@ -24,7 +51,7 @@ export default function FireAstronaut(props: FireAstronautProps) {
           Cancel
         </button>
         <button
-          onClick={props.onClose}
+          onClick={handleFire}
           style={{ backgroundColor: "red", color: "white" }}
           className="modal-button"
         >
